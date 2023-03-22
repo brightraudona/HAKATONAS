5d21daf5e51e450d88a42dda366e78cfb8208868
Matas Togobickij <matas.togobickij@ktu.edu>
2023-03-22 21:55:25 +0200

    Aditional tutorial changes

--- hakatonapp/views.py
+++ hakatonapp/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.utils.timezone import datetime
 
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    return render(
+        request,
+        'hakatonapp/hello_there.html',
+        {
+            'name': name,
+            'date': datetime.now()
+        }
+    )
